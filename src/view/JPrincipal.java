999b08130254f280295ba73975a5bc6e20f15af5
iamjonn <jonata10.jn@gmail.com>
2025-01-21 23:39:59 -0300

    prototipo de tala principal finalizado

--- src/view/JPrincipal.java
+++ src/view/JPrincipal.java
@@ -13,11 +13,17 @@ import java.awt.Color;
 import javax.swing.JTree;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.ImageIcon;
 
 public class JPrincipal extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -47,13 +53,43 @@ public class JPrincipal extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(106, 189, 89, 23);
+		JButton btnNewButton = new JButton("traduzir para portugues");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(133, 185, 147, 23);
 		contentPane.add(btnNewButton);
 		
 		JComboBox comboBox = new JComboBox();
 		comboBox.setModel(new DefaultComboBoxModel(new String[] {"ingles", "espanhol", "alemao"}));
-		comboBox.setBounds(241, 63, 145, 22);
+		comboBox.setBounds(241, 147, 75, 22);
 		contentPane.add(comboBox);
+		
+		JButton btnNewButton_1 = new JButton("traduzir para ");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_1.setBounds(69, 147, 162, 23);
+		contentPane.add(btnNewButton_1);
+		
+		textField = new JTextField();
+		textField.setBounds(170, 83, 329, 36);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(46, 228, 592, 182);
+		contentPane.add(textArea);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon(JPrincipal.class.getResource("/imagens/ingles.png")));
+		lblNewLabel.setBounds(399, 130, 205, 87);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("DIGITE A PALAVRA  QUE QUER TRADUZIR");
+		lblNewLabel_1.setBounds(69, 11, 535, 39);
+		contentPane.add(lblNewLabel_1);
 	}
 }
